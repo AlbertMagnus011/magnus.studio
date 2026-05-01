f84d0ba29a8f11b659eca8583d672a277a478cf4
Marcos Silva <marcosdasilva399@gmail.com>
2026-05-01 15:44:25 -0300

    minor changes to the Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,33 +1,31 @@
-import Link from "next/link";
+import Link from "next/link"
 
-const navItems = [
-  { label: "Sobre", href: "#sobre" },
-  { label: "Tecnologias", href: "#technologies" },
-  { label: "Projetos", href: "#projects" },
-  { label: "Contato", href: "#contact" },
-];
+export const Navbar = () => {
+    const navLinks = [
+        {label:"Sobre", href: "#abbout"},
+        {label:"Tecnologias", href: "#technologies"},
+        {label:"Projetos", href: "#projects"},
+        {label:"Contato", href: "#contact"}
+    ]
 
-export const Navbar = () =>  {
-  return (
-    <header className="sticky top-0 z-50 border-b border-zinc-800 bg-black">
-      <nav className="mx-auto flex h-20 max-w-7xl items-center justify-between px-8 md:px-16">
-        <Link href="/" className="text-2xl tracking-tight text-white">
-          <span className="font-light">const</span>
-          <span className="font-semibold px-2">Magnus</span>
-        </Link>
-        <ul className="flex items-center gap-8">
-          {navItems.map((item) => (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className="relative text-sm font-medium text-zinc-300 transition-colors duration-300 hover:text-white after:absolute
-                  after:-bottom-1 after:left-0 after:h-0.5 after:w-0 after:bg-violet-500 after:transition-all after:duration-300 hover:after:w-full">
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+    <header className="sticky top-0 border-b border-zinc-800 bg-black ">
+        <nav className="container mx-auto flex h-20 items-center justify-between px-8 md:px-16">
+            <Link href="/" className="text-2xl tracking-tight">
+                <span className="font-light text-blue-700">const</span>
+                <span className="px-2 font-semibold text-blue-400">magnus</span>
+            </Link>
+                <ul className="flex items-center gap-8">
+                    {navLinks.map((item) => (
+                        <li key={item.href}>
+                            <Link href={item.href} className="relative text-[#4ADE80] transition-colors duration-300 hover:text-[#FB7185] after:absolute after:-bottom-1 after:h-0.5
+                            after:left-0 after:bg-[#FB7185] after:transition-all after:w-0 after:duration-500 hover:after:w-full">
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    )
 }
